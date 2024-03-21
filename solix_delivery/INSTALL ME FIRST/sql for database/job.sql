07fa41f4c7212171b565e9891c465323f9372791
Solix <81125185+DaydexB@users.noreply.github.com>
2024-03-21 11:39:56 -0500

    Fixed Syntax Error

--- solix_delivery/INSTALL ME FIRST/sql for database/job.sql
+++ solix_delivery/INSTALL ME FIRST/sql for database/job.sql
@@ -1,7 +1,5 @@
 INSERT INTO `jobs` (name, label) VALUES
-	('delivery', 'Delivery')
-;
+    ('delivery', 'Delivery');
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('delivery',0,'driver','Driver',500,'{}','{}'),
-;
+    ('delivery',0,'driver','Driver',500,'{}','{}');
