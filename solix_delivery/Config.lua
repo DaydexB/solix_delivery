63f9194a1c192c7d6cadf0b42ef2eeb8f58c653b
Solix <81125185+DaydexB@users.noreply.github.com>
2024-03-20 23:38:06 -0500

    Update Config.lua

--- solix_delivery/Config.lua
+++ solix_delivery/Config.lua
@@ -51,5 +51,3 @@ Config.Delivery3PedHeading = 298.5440
 Config.Delivery3 = vec3(-1045.7565, -228.4333, 38.0143) 
 
 Config.Delivery3Message = 'Your 3rd ~y~Delivery~w~ Is Now Ready Check Your Map'  -- The Location Where The Ped Will Spawn when the 3rd delivery is active
-
-Config.DontTouch = true
