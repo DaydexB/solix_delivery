e8ce44b03228744c3894bc3c136cae7d5763146a
DaydexB <81125185+DaydexB@users.noreply.github.com>
2024-03-20 22:33:08 -0500

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,55 @@
+Config = {}
+
+Config.ClockOutMessage = 'Your Deliverys Are Finished Go ~y~Clock Out~w~!'
+
+Config.StartJobPedLocation = vec3(-1176.6783, -890.5800, 12.8125)
+
+Config.StartJobPedModel = 'csb_burgerdrug' -- The Ped Model That Will Spawn To Start Your Job
+
+Config.StartJobPedHeading = 302.1301
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Config.BlipName = 'Delivery Job'
+
+Config.BlipColour = 5
+
+Config.BlipId = 479
+
+Config.BlipLocation = {x = -1176.6783, y = -890.5800, z = 12.8125}
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Config.JobVehicle = 'burrito3' -- The Vehicle That Will Spawn For Workers To Deliver
+
+Config.JobVehicleLocation = vector3(-1181.306, -875.490, 13.926)
+
+Config.JobVehicleHeading = 35.0
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Config.TipSystem = true -- true = 'Gives Player a $100 - $200 tip (NOT EVERY DELIVERY ITS RANDOM) prices changable in server.lua'     false = 'Doesnt give Player a tip'
+
+Config.Delivery1PedModel = 'a_f_m_fatwhite_01'
+
+Config.Delivery1PedHeading = 141.47
+
+Config.Delivery1 = vec3(80.1895, -1902.8728, 20.6422)   -- The Location Where The Ped Will Spawn when the 1st delivery is active
+
+Config.Delivery1Message = 'Go To The WayPoint On Your Map A ~y~Delivery~w~ Is Ready' 
+
+Config.Delivery2PedModel = 'csb_ballasog'
+
+Config.Delivery2PedHeading = 31.11
+
+Config.Delivery2 = vec3(-884.2065, -1072.5535, 1.5317)   -- The Location Where The Ped Will Spawn when the 2nd delivery is active
+
+Config.Delivery2Message = 'Your 2nd ~y~Delivery~w~ Is Now Ready Check Your Map'
+
+Config.Delivery3PedModel = 'cs_lifeinvad_01'
+
+Config.Delivery3PedHeading = 298.5440
+
+Config.Delivery3 = vec3(-1045.7565, -228.4333, 38.0143) 
+
+Config.Delivery3Message = 'Your 3rd ~y~Delivery~w~ Is Now Ready Check Your Map'  -- The Location Where The Ped Will Spawn when the 3rd delivery is active
+
+Config.DontTouch = true
