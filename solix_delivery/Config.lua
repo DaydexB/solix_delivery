7b6124cdeebec5836049af2c81aea899c2236ba3
Solix <81125185+DaydexB@users.noreply.github.com>
2024-04-10 16:12:40 -0700

    Update Config.lua

--- solix_delivery/Config.lua
+++ solix_delivery/Config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.ClockOutMessage = 'Your Deliverys Are Finished Go ~y~Clock Out~w~!'
+Config.Subtitle4 = 'Your Deliverys Are Finished Go ~y~Clock Out~w~!'
 
 Config.StartJobPedLocation = vec3(-1176.6783, -890.5800, 12.8125)
 
@@ -34,7 +34,7 @@ Config.Delivery1PedHeading = 141.47
 
 Config.Delivery1 = vec3(80.1895, -1902.8728, 20.6422)   -- The Location Where The Ped Will Spawn when the 1st delivery is active
 
-Config.Delivery1Message = 'Go To The WayPoint On Your Map A ~y~Delivery~w~ Is Ready' 
+Config.Subtitle1 = 'Go To The WayPoint On Your Map A ~y~Delivery~w~ Is Ready' 
 
 Config.Delivery2PedModel = 'csb_ballasog'
 
@@ -42,7 +42,7 @@ Config.Delivery2PedHeading = 31.11
 
 Config.Delivery2 = vec3(-884.2065, -1072.5535, 1.5317)   -- The Location Where The Ped Will Spawn when the 2nd delivery is active
 
-Config.Delivery2Message = 'Your 2nd ~y~Delivery~w~ Is Now Ready Check Your Map'
+Config.Subtitle2 = 'Your 2nd ~y~Delivery~w~ Is Now Ready Check Your Map'
 
 Config.Delivery3PedModel = 'cs_lifeinvad_01'
 
@@ -50,4 +50,4 @@ Config.Delivery3PedHeading = 298.5440
 
 Config.Delivery3 = vec3(-1045.7565, -228.4333, 38.0143) -- The Location Where The Ped Will Spawn when the 3rd delivery is active
 
-Config.Delivery3Message = 'Your 3rd ~y~Delivery~w~ Is Now Ready Check Your Map' 
+Config.Subtitle3 = 'Your 3rd ~y~Delivery~w~ Is Now Ready Check Your Map' 
